496ed5df62c9cca54597d201590dbcc0ca7ca3aa
Povilas <pbrilius@gmail.com>
2021-01-09 20:01:21 +0200

    Composite structure of user-text note relationship.

--- app/Models/TextNote.php
+++ app/Models/TextNote.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use App\Entities\TextNote;
 
 class TextNote extends Model
 {
   protected $allowedFields = ['label', 'text', 'user_id'];
   protected $useTimestamps = true;
+
+  protected $returnType = TextNote::class;
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Entity\User as EntityUser;
 use CodeIgniter\Model;
 
 class User extends Model
 {
   protected $allowedFields = ['email','first_name','last_name'];
   protected $useTimestamps = true;
+
+  protected $returnType = EntityUser::class;
 }
