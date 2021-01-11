f6e5840bf3a97d7b2fde575481f724a9ac8934d5
Povilas <pbrilius@gmail.com>
2021-01-11 13:14:40 +0200

    Text note face fix.

--- app/Models/TextNote.php
+++ app/Models/TextNote.php
@@ -3,12 +3,12 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
-use App\Entities\TextNote;
+use App\Entities\TextNoteE as TextNoteEntity;
 
 class TextNote extends Model
 {
   protected $allowedFields = ['label', 'text', 'user_id'];
   protected $useTimestamps = true;
 
-  protected $returnType = TextNote::class;
+  protected $returnType = TextNoteEntity::class;
 }
