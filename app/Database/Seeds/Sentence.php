5a6231266900d91b5d44e072a6bd37c7f06fd08f
Povilas <pbrilius@gmail.com>
2021-01-13 17:24:50 +0200

    Sentence seed.

---
+++ app/Database/Seeds/Sentence.php
@@ -0,0 +1,15 @@
+<?php namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class Sentence extends Seeder
+{
+	public function run()
+	{
+		$data = [
+			'text_note_id' => $this->db->query('SELECT * FROM ci4_text_note LIMIT 1')->getResultObject()[0]->id,
+			'order_no' => 1,
+			'sentence' => $this->db->query('SELECT * FROM ci4_text_note LIMIT 1')->getResultObject()[0]->text,
+		];
+	}
+}
